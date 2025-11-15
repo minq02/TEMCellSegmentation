d9b4f12b07dba295bf337adb63984825514a0711
Min Kyu Kim <andrewkimminkyu@gmail.com>
2025-11-15 00:17:39 -0500

    init layout

---
+++ src/datasets/tem_dataset.py
@@ -0,0 +1,9 @@
+# src/datasets/tem_dataset.py
+import h5py
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+class TEMDataset(Dataset):
+    def __init__(self):
+        pass
